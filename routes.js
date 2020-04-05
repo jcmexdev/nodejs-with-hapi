39b0599d09afdbf692b1a39a7a44feac0e64cd73
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-04-05 17:49:43 -0500

    Validando la información - Implementando Joi

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+const Joi = require('@hapi/joi');
 const { register, home } = require('./controllers/site');
 const { createUser } = require('./controllers/user');
 
@@ -14,6 +15,15 @@ module.exports = [
   },
   {
     method: 'POST',
+    options: {
+      validate: {
+        payload: Joi.object({
+          name: Joi.string().required().min(3),
+          email: Joi.string().email().required(),
+          password: Joi.string().required().min(6),
+        }),
+      },
+    },
     path: '/create-user',
     handler: createUser,
   },
