c20d1891e51e6f03d4d9429792bbd7fb18510cc3
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-04-05 14:59:56 -0500

    Renderizado de vistas - Layout y template del home

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 const Hapi = require('@hapi/hapi');
+const handlebars = require('handlebars');
 const inert = require('@hapi/inert');
 const path = require('path');
+const vision = require('@hapi/vision');
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
@@ -16,12 +18,25 @@ const server = Hapi.server({
 async function init() {
   try {
     await server.register(inert);
+    await server.register(vision);
+
+    server.views({
+      engines: {
+        hbs: handlebars,
+      },
+      relativeTo: __dirname,
+      path: 'views',
+      layout: true,
+      layoutPath: 'views',
+    });
 
     server.route({
       method: 'GET',
-      path: '/home',
+      path: '/',
       handler: (req, h) => {
-        return h.file('index.html');
+        return h.view('index', {
+          title: 'home',
+        });
       },
     });
 
