88cb058f15107fb9f609652f82c59b38e1f0d15f
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-04-06 19:16:01 -0500

    Repaso - Creación del modelo y controlador para preguntas

---
+++ controllers/question.js
@@ -0,0 +1,25 @@
+'use strict';
+const { questions } = require('../models/index');
+
+async function createQuestion(req, h) {
+  let { payload, state } = req;
+  let result;
+  try {
+    result = await questions.create(payload, state.user);
+    console.log(`Pregunta creada con el id: ${result}`);
+  } catch (error) {
+    console.error(error);
+    return h
+      .view('ask', {
+        title: 'Crear pregunta',
+        error: 'Problemas creando la pregunta',
+      })
+      .code(500)
+      .takeover();
+  }
+  return h.response(`Pregunta creada con el id: ${result}`).code(201);
+}
+
+module.exports = {
+  createQuestion,
+};
---
+++ models/questions.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class Questions {
+  constructor(db) {
+    this.db = db;
+    this.ref = this.db.ref('/');
+    this.collection = this.ref.child('questions');
+  }
+
+  async create(data, user) {
+    const question = { ...data, owner: user };
+    const newQuestion = this.collection.push();
+    newQuestion.set(question);
+    return newQuestion.key;
+  }
+}
+
+module.exports = Questions;
